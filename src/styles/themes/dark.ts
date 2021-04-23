9774133b9a593f6814ee1b3d9526dfb158786fc7
Kaluã Bentes <kaluanbentes@gmail.com>
2021-04-22 22:39:19 -0400

    Create themes

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+const darkTheme = {
+  colors: {
+    background: "#05121f",
+    surface: "#0c1d2e",
+    text: "#ffffff",
+    textMuted: "#485565",
+    primary: "#3366ff",
+  },
+};
+
+export default darkTheme;
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+const lightTheme = {
+  colors: {
+    background: "#f7f8fa",
+    surface: "#ffffff",
+    text: "#3d4957",
+    textMuted: "#b3b9c3",
+    primary: "#3366ff",
+  },
+};
+
+export default lightTheme;
